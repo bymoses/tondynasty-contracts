c7fafa51246b6900e2124c1754c24bd096caeb86
mml00 <mml00.work@gmail.com>
2024-06-06 18:45:39 +0300

    chore(upgradable): early exit implemented

--- tests/UpgradableExample.spec.ts
+++ tests/UpgradableExample.spec.ts
@@ -23,6 +23,13 @@ describe('UpgradableExample', () => {
         v2 = blockchain.openContract(UpgradableExampleV2.fromAddress(v1.address));
         v3 = blockchain.openContract(UpgradableExampleV3.fromAddress(v1.address));
 
+        // blockchain.verbosity = {
+        //     print: true,
+        //     blockchainLogs: true,
+        //     vmLogs: 'vm_logs_full',
+        //     debugLogs: true,
+        // }
+
         const deployResult = await v1.send(
             deployer.getSender(),
             {
@@ -47,41 +54,48 @@ describe('UpgradableExample', () => {
         // blockchain and nFTCollection are ready to use
     });
 
-    it('v1: should increment counter', async () => {
-        await v1.send(deployer.getSender(), { value: toNano('1') }, 'Increment');
-        const counter = await v1.getCounter();
-        expect(counter).toEqual(1n);
-    })
-
-    it('v1: should upgrade to v2 with "Double" message receiver', async () => {
-        const newContract = await UpgradableExampleV2.fromInit(deployer.address)
-        const upgradeContract: UpgradeContract = {
-            $$type: 'UpgradeContract',
-            code: newContract.init!.code,
-            data: null,
-        }
-        const upgradeResult = await v1.send(deployer.getSender(), { value: toNano('1') }, upgradeContract)
-        expect(upgradeResult.transactions).toHaveTransaction({
-            from: deployer.address,
-            to: v1.address,
-            success: true,
-        })
-
-        await v2.send(deployer.getSender(), { value: toNano('1') }, 'Increment');
-        let counterBefore = await v2.getCounter();
-
-        const doubleResult = await v2.send(deployer.getSender(), { value: toNano('1') }, 'Double')
-        expect(doubleResult.transactions).toHaveTransaction({
-            from: deployer.address,
-            to: v1.address,
-            success: true,
-        })
-
-        const counterAfter = await v2.getCounter();
-        expect(counterAfter).toEqual(counterBefore * 2n);
-    });
+    // it('v1: should increment counter', async () => {
+    //     await v1.send(deployer.getSender(), { value: toNano('1') }, 'Increment');
+    //     const counter = await v1.getCounter();
+    //     expect(counter).toEqual(1n);
+    // })
+    //
+    // it('v1: should upgrade to v2 with "Double" message receiver', async () => {
+    //     const newContract = await UpgradableExampleV2.fromInit(deployer.address)
+    //     const upgradeContract: UpgradeContract = {
+    //         $$type: 'UpgradeContract',
+    //         code: newContract.init!.code,
+    //         data: null,
+    //     }
+    //     const upgradeResult = await v1.send(deployer.getSender(), { value: toNano('1') }, upgradeContract)
+    //     expect(upgradeResult.transactions).toHaveTransaction({
+    //         from: deployer.address,
+    //         to: v1.address,
+    //         success: true,
+    //     })
+    //
+    //     await v2.send(deployer.getSender(), { value: toNano('1') }, 'Increment');
+    //     let counterBefore = await v2.getCounter();
+    //
+    //     const doubleResult = await v2.send(deployer.getSender(), { value: toNano('1') }, 'Double')
+    //     expect(doubleResult.transactions).toHaveTransaction({
+    //         from: deployer.address,
+    //         to: v1.address,
+    //         success: true,
+    //     })
+    //
+    //     const counterAfter = await v2.getCounter();
+    //     expect(counterAfter).toEqual(counterBefore * 2n);
+    // });
 
     it('v2: should upgrade to v3 with "getVersion" getter', async () => {
+        // 1. Increment counter 0 -> 1
+        await v2.send(deployer.getSender(), { value: toNano('1') }, 'Increment')
+
+        // Should be 1
+        console.log("----> counter before upgrade", await v2.getCounter());
+
+        // 2. Upgrading contract
         const newContract = await UpgradableExampleV3.fromInit(deployer.address)
         const upgradeContract: UpgradeContract = {
             $$type: 'UpgradeContract',
@@ -89,16 +103,18 @@ describe('UpgradableExample', () => {
             data: newContract.init!.data,
         }
         const result = await v2.send(deployer.getSender(), { value: toNano('1') }, upgradeContract)
-        console.log(result)
         expect(result.transactions).toHaveTransaction({
             from: deployer.address,
             to: v1.address,
             success: true,
         })
-        console.log("counter", await v2.getCounter());
 
-        const version = await v3.getVersion();
-        console.log(version)
-        expect(version).toEqual(3n);
+        // 3. Checking counter after upgrade. Should be 1, but overwritten to 0
+        console.log("----> counter after upgrade", await v2.getCounter());
+
+        // await v2.send(deployer.getSender(), { value: toNano('1') }, 'Increment')
+        // const version = await v3.getVersion();
+        // console.log(version)
+        // expect(version).toEqual(3n);
     });
 });
